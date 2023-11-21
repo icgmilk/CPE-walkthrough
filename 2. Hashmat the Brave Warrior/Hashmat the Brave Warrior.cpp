267587ba07948afa552773b6d45f9945de591a8b
Jim <example.gmail.com>
2023-11-21 23:29:44 +0800

    add 2.

---
+++ 2. Hashmat the Brave Warrior/Hashmat the Brave Warrior.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long int army, opponent;
+    while(cin >> army >> opponent){
+    	cout << abs(army - opponent) << endl;
+    }
+    
+    
+}
