96ee3a746586841d9b26c3bcb7e74ad376eb6d3b
Jim <example.gmail.com>
2023-11-27 02:56:12 +0800

    add 8.

---
+++ 8. What's Cryptanalysis/main.cpp
@@ -0,0 +1,35 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int main(){
+    int input, ACIIListCount[150] = {0}, letterCount = 0;
+    cin >> input;
+    cin.ignore();
+    
+    while(input != 0){
+        string str;
+        getline(cin, str);
+
+        for(int i = 0; i < str.length(); i++){
+            if (str[i] >= 'a' && str[i] <= 'z'){
+                ACIIListCount[str[i] - 'a' + 'A']++;
+                letterCount++;
+            }
+            else{
+                ACIIListCount[str[i]]++;
+                letterCount++;
+            }
+        }
+        input--;
+    }
+    
+    for(int i = letterCount; i >= 1; i--){
+        for(char j = 'A'; j <= 'Z'; j++){
+            if(i == ACIIListCount[j]){
+                cout << j << ' ' << ACIIListCount[j] << endl;
+            }
+        }
+    }
+    
+}
