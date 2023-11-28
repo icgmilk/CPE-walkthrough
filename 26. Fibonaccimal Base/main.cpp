c445089c3e7aea152a242db312d6687bbbff2ecf
KiDIOda <jimasdfghjkl123456789@gmail.com>
2023-11-28 08:52:23 +0800

    2023/11/28

---
+++ 26. Fibonaccimal Base/main.cpp
@@ -0,0 +1,34 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int main(){
+    int fib[40] = {0, 1};
+    for(int i = 2; i < 40; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    
+    int testCases;
+    cin >> testCases;
+    
+    while(testCases--){
+        int numbers;
+        bool isFibBase = false;
+        cin >> numbers;
+        cout << numbers << " = ";
+        
+        for(int i = 39; i > 1; i--){
+            if(numbers >= fib[i]){
+                cout << '1';
+                numbers -= fib[i];
+                isFibBase = true;
+            }
+            else if(isFibBase == true){
+                cout << '0';
+            }
+        }
+        
+        cout << " (fib)" << endl;
+    }
+    
+}
---
+++ 27. Funny Encryption Method/main.cpp
@@ -0,0 +1,40 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int decToHex(int m){
+    int hexNumber = 0, index = 0;
+    while(m != 0){
+        hexNumber += pow(16, index) * (m % 10);
+        m /= 10;
+        index++;
+    }
+    
+    return hexNumber;
+}
+
+int binaryCount(int m){
+    int onesCount = 0;
+    while(m != 0){
+        if(m % 2 == 1){
+            onesCount++;
+        }
+        
+        m /= 2;
+    }
+    
+    return onesCount;
+}
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    while(n--){
+        int m;
+        cin >> m;
+        cout << binaryCount(m) << ' ' << binaryCount(decToHex(m)) << endl;
+    }
+}
